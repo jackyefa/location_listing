92a3e5ec7e7448d8704017f86cd1a672297063ae
Sanjay V <vishwasanjay07@gmail.com>
2018-05-28 17:10:26 +0530

    Added root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'welcome#index'
   namespace :api do 
     namespace :v1 do       
-      # sessions
+      # sessions      
       post 'sign_in', :to => 'sessions#sign_in' 
       post 'sign_up', :to => 'sessions#sign_up'    
       post 'forgot_password', :to => 'sessions#forgot_password' 
