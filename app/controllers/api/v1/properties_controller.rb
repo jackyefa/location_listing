d5e5bcf1ae2e6e534864ac7020431cdc757d5a8b
Sanjay V <vishwasanjay07@gmail.com>
2018-07-10 17:36:23 +0530

    Return property id in response.

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::PropertiesController <  Api::V1::BaseController
   before_action :authenticate_user!
 
-  def index     
+  def index   
+    
     @properties = Property.all_except(@current_user)
     @user_properties = @current_user.properties
   end
@@ -12,9 +13,9 @@ class Api::V1::PropertiesController <  Api::V1::BaseController
       if params[:property][:image].present?
         decoded_file = decode_base64_image(params[:property][:image].gsub(/\\n/, "\n").gsub(/\\t/, "\t").gsub(/\\r/, "\r"))
         picture = @property.pictures.create(image: decoded_file)
-        render_success and return
+        render json: {success: true, id: @property.id}, status: 200 and return
       end
-      render_success
+      render json: {success: true, id: @property.id}, status: 200
     else
       render_with_errors(@property.errors.full_messages.join(','))
     end
